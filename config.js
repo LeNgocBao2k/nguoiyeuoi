312cb1d38a11ef6779e3db0a299afaec3772c467
LeNgocBao2k <101648457+LeNgocBao2k@users.noreply.github.com>
2022-07-01 20:40:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Hee luu cậu!',
-    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    introTitle: 'Hee luu Hương!',
+    introDesc: `Tớ có điều này muốn hỏi cậu, Hương nhớ phải trả lời thật lòng nha`,
     btnIntro: 'oke',
-    title: 'Cậu yêu tớ có phải không 😙',
+    title: 'Hương yêu Dương đúng không 😙',
     desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là vợ tớ đó nha :> ',
     btnYes: 'yêu ơi là iuuu <33',
-    btnNo: 'cậu mơ à :333',
+    btnNo: 'Dương mơ à :333',
     question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3',
     btnReply: 'Gửi cho tớ <3',
-    reply: 'Tại vì Bảo quá đẹp trai đó hihi <33333333',
+    reply: 'Tại vì Dương quá đẹp trai đó hihi <33333333',
     mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
-    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho Dương đi nào.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://facebook.com/lengocbao.io' 
+    messLink: 'https://www.facebook.com/profile.php?id=100054906660509' 
 }
